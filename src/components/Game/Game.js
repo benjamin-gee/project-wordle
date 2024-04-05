6bfe7d9ff541c50b67a74267f1653f9833a5e13e
Benjamin Gee <benjamin.n.gee@gmail.com>
2024-04-05 14:09:33 -0600

    Sad banner if game wasn't won

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -8,11 +8,10 @@ import GuessGrid from "../GuessGrid/GuessGrid";
 import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 import { checkGuess } from "../../game-helpers";
 import HappyBanner from "../HappyBanner/HappyBanner";
+import SadBanner from "../SadBanner/SadBanner";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
-// To make debugging easier, we'll log the solution in the console.
-console.info({ answer });
 
 function Game() {
     const [guesses, setGuesses] = React.useState([]);
@@ -36,7 +35,7 @@ function Game() {
     return (
         <>
             {foundAnswer && <HappyBanner numOfGuesses={guesses.length} />}
-            {!foundAnswer && outOfGuesses && <div>you lost!</div>}
+            {!foundAnswer && outOfGuesses && <SadBanner answer={answer} />}
             <GuessGrid guesses={guesses} answer={answer} />
             <PreviousGuessList guesses={guesses} />
             <GuessInput
